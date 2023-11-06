9031f86f4b0e81123fa6715ae00d2397a3911c89
quevedito2 <u201619823@upc.edu.pe>
2023-11-06 00:11:19 -0500

    Consumo de API - concluido

--- lib/vista1.dart
+++ lib/vista1.dart
@@ -15,7 +15,31 @@ class vista1 extends StatelessWidget {
         initialData: [],
         future: Service.getUsers(),
         builder: (context, AsyncSnapshot<List> snapshot) {
-          
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              var usuario = snapshot.data![index];
+              return Container(
+                height: 100,
+                child: Column(children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 250,
+                        child: Column(children: [
+                          Text('ID: ${usuario.id}'),
+                          Text('Last Name: ${usuario.lastName}'),
+                          Text('First Name: ${usuario.firstName}'),
+                          Text('Email: ${usuario.email}'),
+                        ]),
+                      ),
+                      Image.network('${usuario.avatar}', width: 70, height: 50),
+                    ],
+                  )
+                ]),
+              );
+            },
+          );
         },
       ),
     );
